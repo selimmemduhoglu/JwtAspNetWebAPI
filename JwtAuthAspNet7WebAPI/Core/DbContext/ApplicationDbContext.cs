17ca5d185e7a7b7f0afd7e3e04131fe780aee040
MuhammedSelimMemduho <MuhammedSelimMemduho@DESKTOP-B530L54>
2024-02-19 15:29:13 +0300

    Add project files.

---
+++ JwtAuthAspNet7WebAPI/Core/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using JwtAuthAspNet7WebAPI.Core.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace JwtAuthAspNet7WebAPI.Core.DbContext
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+
+                optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            }
+        }
+    }
+}
---
+++ JwtAuthAspNet7WebAPI/Core/Dtos/RegisterDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JwtAuthAspNet7WebAPI.Core.Dtos
+{
+    public class RegisterDto
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+
+    }
+}
